b2b515ef746f6fa5ac00782c4cc323c07a640d1e
bunterssmv <marv12.h6@gmail.com>
2025-12-02 11:06:32 +0000

    Changes to be committed: 	modified:   App.java

--- App.java
+++ App.java
@@ -1,3 +1,9 @@
 public class App {
     ThreadGroup tg = new ThreadGroup("Clientes");
+    run() {
+        Thread t1 = new Thread(tg, new Cliente("Cliente 1"));
+        Thread t2 = new Thread(tg, new Cliente("Cliente 2"));
+        t1.start();
+        t2.start();
+    }
 }
