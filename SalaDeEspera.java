a51cae443ed64bce268a14b6e1c5d5d6521acbc0
EselRawi <raulgggafas@gmail.com>
2025-12-02 13:06:06 +0100

    modified:   SalaDeEspera.java

--- SalaDeEspera.java
+++ SalaDeEspera.java
@@ -1,3 +1,42 @@
+import java.util.LinkedList;
+
 public class SalaDeEspera {
-    
+
+    private int numSillas;
+    private LinkedList<Cliente> cola;
+
+    public SalaDeEspera(int numSillas) {
+        this.numSillas = numSillas;
+        this.cola = new LinkedList<>();
+    }
+
+    // intenta sentarse y si sale tru se une a la cola fifo y si la cola esta llena devuelve false
+    public synchronized boolean entrar(Cliente c) {
+
+        if (cola.size() == numSillas) {
+            return false; 
+        }
+
+        cola.addLast(c); 
+        notifyAll();     // avisamos a los barberos por que puede que esten dormidos
+        return true;
+    }
+
+    // El barbero duerme hasta que haya un cliente cuando lo hay saca el primero de la lista
+    public synchronized Cliente siguienteCliente() {
+
+        while (cola.isEmpty()) {
+            try {
+                wait(); // barbero duerme
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        return cola.removeFirst(); 
+    }
 }
+
+
+    
+
