ca349e7ddae0f8a40b1f7b5adaa968b0cbf200a3
bunterssmv <marv12.h6@gmail.com>
2025-12-02 12:07:01 +0000

    modified:   Barbero.java 	modified:   Cliente.java

--- Barbero.java
+++ Barbero.java
@@ -1,3 +1,7 @@
 public class Barbero {
+    threadGrupo barberoGrupo = new threadGrupo("BarberoGrupo");
+
+    thread barbero0Thread = new thread(barberoGrupo, new BarberoRunnable(), "Barbero0Thread");
+    thread barbero1Thread = new thread(barberoGrupo, new BarberoRunnable(), "Barbero1Thread");
     
 }
